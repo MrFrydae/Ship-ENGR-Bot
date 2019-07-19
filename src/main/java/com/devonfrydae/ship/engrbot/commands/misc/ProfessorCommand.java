320904d2df3b578332c4847c3514ca163c9c6688
mjangle1985 <mjangle1985@gmail.com>
2019-07-19 16:54:11 -0400

    updated prof commit

--- src/main/java/com/devonfrydae/ship/engrbot/commands/misc/ProfessorCommand.java
+++ src/main/java/com/devonfrydae/ship/engrbot/commands/misc/ProfessorCommand.java
@@ -3,6 +3,7 @@ package com.devonfrydae.ship.engrbot.commands.misc;
 
 import com.devonfrydae.ship.engrbot.commands.BotCommand;
 import com.devonfrydae.ship.engrbot.commands.Command;
+import com.devonfrydae.ship.engrbot.commands.CommandEvent;
 import com.devonfrydae.ship.engrbot.commands.CommandType;
 import com.devonfrydae.ship.engrbot.containers.Professor;
 import com.devonfrydae.ship.engrbot.utils.CSVUtil;
@@ -17,11 +18,14 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class ProfessorCommand extends Command {
 
-    @Override
-    public void onCommand(MessageReceivedEvent event, String[] args) {
-        String professorName = args[0];
-        Professor professor = CSVUtil.getProfessors(professorName);
+//    @Override
+//    public void onCommand(MessageReceivedEvent event, String[] args) {
+//        String professorName = args[0];
+//        Professor professor = CSVUtil.getProfessors(professorName);
+//    }
 
+    @Override
+    public void onCommand(CommandEvent event) {
 
     }
 }
